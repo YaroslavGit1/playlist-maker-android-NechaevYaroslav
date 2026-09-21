87bd17ad76735f30eb182ab1a6875db8fd054e3a
YaroslavGit1 <jark2016@mail.ru>
2026-09-21 16:57:40 +0600

    feat: project-sprint04

---
+++ app/src/main/java/com/example/playlist_maker_android_nechaevyaroslav/ui/navigation/PlaylistScreen.kt
@@ -0,0 +1,7 @@
+package com.example.playlist_maker_android_nechaevyaroslav.ui.navigation
+
+enum class PlaylistScreen(val route: String) {
+    Main("main"),
+    Search("search"),
+    Settings("settings"),
+}
