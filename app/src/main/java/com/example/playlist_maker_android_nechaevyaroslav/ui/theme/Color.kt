65bae84a46a6c162af191924d25f4c80bdc71259
YaroslavGit1 <jark2016@mail.ru>
2026-09-21 15:01:01 +0600

    feat: project-sprint03

--- app/src/main/java/com/example/playlist_maker_android_nechaevyaroslav/ui/theme/Color.kt
+++ app/src/main/java/com/example/playlist_maker_android_nechaevyaroslav/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Blue = Color(0xFF3772E7)
+val Black = Color(0xFF1A1B22)
+val Gray = Color(0xFFAEAFB4)
+val White = Color(0xFFFFFFFF)
